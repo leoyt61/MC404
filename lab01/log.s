cf1d75365c0ee7fba2c9c9020d3783cf692c12d1
leoyt61 <leonardo_yoshida@yahoo.com.br>
2020-04-28 20:17:13 -0300

    lab 1 add

---
+++ lab01/log.s
@@ -0,0 +1,32 @@
+	.file	"log.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%d rocks\n"
+	.text
+	.globl	my_print
+	.type	my_print, @function
+my_print:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	my_print, .-my_print
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ lab01/math.s
@@ -0,0 +1,25 @@
+	.file	"math.c"
+	.text
+	.globl	my_sum
+	.type	my_sum, @function
+my_sum:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	my_sum, .-my_sum
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
